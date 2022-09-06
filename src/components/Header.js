2e21b2a359190d5b5cb046074bbd1c14f6a791ae
Jaydeep Patel <58411819+jaydeep-patel-au6@users.noreply.github.com>
2022-09-06 19:13:47 +0530

    import font

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,11 @@ import Logo from "../assets/images/logo.svg";
 const Header = () => {
   return (
     <div className="bg-black w-[802px] h-[52px] flex items-center justify-between">
-      <p className="text-white pl-[43.77px]">Amis du Festival</p>
-      <div className="pr-[29.31px]">
-        <img src={Logo} />
+      <p className="text-white pl-[43.77px] text-base leading-4 font-normal not-italic">
+        Amis du Festival
+      </p>
+      <div className="pr-[29.31px] w-[214.87px] h-[38.9px]">
+        <img src={Logo} className="object-cover" />
       </div>
     </div>
   );
