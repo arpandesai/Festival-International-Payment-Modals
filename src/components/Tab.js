ba123e551d5b87076f06c71a5f2d6913ca969391
Jaydeep Patel <58411819+jaydeep-patel-au6@users.noreply.github.com>
2022-09-06 20:29:10 +0530

    create tab reusable component

---
+++ src/components/Tab.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Tab = ({ title, buttonHeightandWidth, selectElement }) => {
+  return (
+    <div
+      className={`cursor-pointer mr-[14px] ${
+        buttonHeightandWidth ? "w-[216px] h-[31px]" : null
+      }  rounded-[3px] 
+        ${
+          selectElement === "selected"
+            ? "bg-[#D0D0D0] buttonBorder"
+            : "bg-[#F3F3F3] hover:bg-[#E4E4E4]"
+        }  flex items-center justify-center`}
+    >
+      <p
+        className={`text-xs leading-3 text-black ${
+          selectElement === "selected" ? "font-bold" : "font-normal"
+        } capitalize`}
+      >
+        {title}
+      </p>
+    </div>
+  );
+};
+
+export default Tab;
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import { headerButtonTitle } from "../assets/data";
 import Header from "../components/Header";
+import Tab from "../components/Tab";
 
 const Home = () => {
+  const [selectElement, setSelectElement] = useState(0);
+  const handleClick = (idx) => {
+    setSelectElement(idx);
+  };
   return (
-    <div className="flex items-center justify-center">
+    <div className="flex items-center justify-center flex-col">
       <Header />
+      <div className="font-bold text-black text-sm leading-[14px] mt-[24.15px] mb-[18.96px] capitalize">
+        <p>choose your donation frequency</p>
+      </div>
+      <div className="flex items-center justify-center flex-row">
+        {headerButtonTitle.map((data, idx) => {
+          return (
+            <div key={idx} onClick={() => handleClick(idx)}>
+              <Tab
+                selectElement={`${selectElement === idx ? "selected" : null}`}
+                title={data?.title}
+                buttonHeightandWidth
+              />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
