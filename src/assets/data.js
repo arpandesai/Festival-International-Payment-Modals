e9974b6ebf6fb729341e767d0f927800b5902fac
Jaydeep Patel <58411819+jaydeep-patel-au6@users.noreply.github.com>
2022-09-07 16:08:23 +0530

    json file complete

--- src/assets/data.js
+++ src/assets/data.js
@@ -180,3 +180,221 @@ export const oneTime = [
     ],
   },
 ];
+
+export const monthly = [
+  {
+    donation: "$5",
+    title: "$5 Perks",
+    subTitle: "Bon",
+    sideText: [
+      {
+        text: "Ami Stickers",
+      },
+      {
+        text: "Ami T-Shirts",
+      },
+      {
+        text: "10% discount on all Festival Merch",
+      },
+      {
+        text: "100% Tax Deductible",
+      },
+    ],
+  },
+  {
+    donation: "$15",
+    title: "$15 Perks",
+    subTitle: "Grand",
+    sideText: [
+      {
+        text: "Ami Stickers",
+      },
+      {
+        text: "Ami T-Shirt",
+      },
+      {
+        text: "10% discount on all Festival Merch",
+      },
+      {
+        text: "Tax deductible less $50",
+      },
+      {
+        text: "Lagniappe Pass",
+      },
+      {
+        text: "Listing on Festival website",
+      },
+    ],
+  },
+  {
+    donation: "$30",
+    title: "$30 Perks",
+    subTitle: "Tres Bon",
+    sideText: [
+      {
+        text: "Ami Stickers",
+      },
+      {
+        text: "10% discount on all Festival Merch",
+      },
+      {
+        text: "Tax deductible less $100",
+      },
+      {
+        text: "2 Lagniappe Pass",
+      },
+      {
+        text: "Listing on Festival website",
+      },
+    ],
+  },
+  {
+    donation: "$50",
+    title: "$50 Perks",
+    subTitle: "Magnifique",
+    sideText: [
+      {
+        text: "Ami Stickers",
+      },
+      {
+        text: "Ami T-Shirts",
+      },
+      {
+        text: "10% discount on all Festival Merch",
+      },
+      {
+        text: "Tax deductible less $100",
+      },
+      {
+        text: "2 Bon Temp Passes",
+      },
+      {
+        text: "Listing on Festival website",
+      },
+      {
+        text: "Listing on Official Festival guide",
+      },
+      {
+        text: "Recognition during Festival International 2023",
+      },
+      {
+        text: "Festival Pin",
+      },
+      {
+        text: "Festival Poster",
+      },
+    ],
+  },
+];
+
+export const annually = [
+  {
+    donation: "$50",
+    title: "$50 Perks",
+    subTitle: "Bon",
+    sideText: [
+      {
+        text: "Ami Stickers",
+      },
+      {
+        text: "Ami T-Shirts",
+      },
+      {
+        text: "10% discount on all Festival Merch",
+      },
+      {
+        text: "10% discount on all Festival Passes",
+      },
+      {
+        text: "100% Tax Deductible",
+      },
+    ],
+  },
+  {
+    donation: "$100",
+    title: "$100 Perks",
+    subTitle: "Grand",
+    sideText: [
+      {
+        text: "Ami Stickers",
+      },
+      {
+        text: "Ami T-Shirt",
+      },
+      {
+        text: "10% discount on all Festival Merch",
+      },
+      {
+        text: "Tax deductible less $50",
+      },
+      {
+        text: "Lagniappe Pass",
+      },
+      {
+        text: "Listing on Festival website",
+      },
+    ],
+  },
+  {
+    donation: "$250",
+    title: "$250 Perks",
+    subTitle: "Tres Bon Ami",
+    sideText: [
+      {
+        text: "Ami Stickers",
+      },
+      {
+        text: "Ami T-Shirt",
+      },
+      {
+        text: "10% discount on all Festival Merch",
+      },
+      {
+        text: "Tax deductible less $100",
+      },
+      {
+        text: "2 Lagniappe Pass",
+      },
+      {
+        text: "Listing on Festival website & in festival Guide",
+      },
+      {
+        text: "Stage Recognation During festival 2022",
+      },
+    ],
+  },
+  {
+    donation: "$500",
+    title: "$500 Perks",
+    subTitle: "Magnifique Ami",
+    sideText: [
+      {
+        text: "Ami Stickers",
+      },
+      {
+        text: "Ami T-Shirts",
+      },
+      {
+        text: "10% discount on all Festival Merch",
+      },
+      {
+        text: "10% discount on all Festival Passes",
+      },
+      {
+        text: "Tax deductible less $300",
+      },
+      {
+        text: "2022 Poster",
+      },
+      {
+        text: "2022 Pin",
+      },
+      {
+        text: "Stage recognition during Festival 2022",
+      },
+      {
+        text: "Listing on Festival Website and in Festival Guide",
+      },
+    ],
+  },
+];
